d66c3f94e6b7f4bcb4f08979c6da59bc5fc38ab2
Christopher Wood <christopherwood07@gmail.com>
2015-12-04 11:03:23 -0800

    partial upate

--- src/adapter.hs
+++ src/adapter.hs
@@ -4,4 +4,14 @@ module Adapter (
     , connectToCcnLite
 ) where
 
+import System.IO
+import Network.Socket 
+
 -- TODO: implement these functions -- they should all return sockets
+
+openSocket :: String -> String -> IO Network.Socket.Socket
+openSocket host port = do
+    (serverAddr:_) <- getAddrInfo Nothing (Just host) (Just port)
+    s <- socket (addrFamily serverAddr) Stream defaultProtocol
+    connect s
+    return s
