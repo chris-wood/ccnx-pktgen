09b458dd3eda54250a8bd2fdf69e15a7ced07f19
Christopher Wood <christopherwood07@gmail.com>
2015-12-04 11:23:59 -0800

    Updating the interest benchmark

--- src/interests.hs
+++ src/interests.hs
@@ -1,14 +1,22 @@
 import Parser
 import System.Environment
+import System.Random
 
 -- Criterion benchmarking library
 import Criterion.Main
 
 -- criterion tutorial: http://www.serpentine.com/criterion/tutorial.html
 
+randomIntStream :: (Random a) => Int -> [a]
+randomIntStream seed = randoms (mkStdGen seed)
+
+_modSwap a b = mod b a
+randomInts :: Int -> Int -> Int -> [Int]
+randomInts n low high = Prelude.take n (Prelude.map (+ low) (Prelude.map (_modSwap (high - low)) (randomIntStream 42)))
+
 main = do
-    args <- getArgs
-    putStrLn $ (read $ head args :: Int)
+--    (low:high:args) <- getArgs
+    
 
     -- TODO: command line input:
     -- step: step for benchmark
@@ -16,7 +24,8 @@ main = do
 
     defaultMain $ [
         bgroup "interests" [
-                bench "1"  $ whnf produceInterests [1]
-                , bench "5"  $ whnf produceInterests [1,2,3,4,5]
+                bench "1"  $ whnf produceInterests (randomInts 1 1 10)
+                , bench "50"  $ whnf produceInterests (randomInts 50 1 10)
+                , bench "100" $ whnf produceInterests (randomInts 100 1 10)
             ]
         ]
