ead3f9ef27069e383b0afd6154ba6ddf5f8abb1f
Christopher Wood <christopherwood07@gmail.com>
2015-12-12 12:12:44 -0800

    name and hash parsing

--- src/receiver.c
+++ src/receiver.c
@@ -84,72 +84,142 @@ _loadDirectory(UDPServer *server, char *directory)
     }
 }
 
+static size_t
+_getNameLength(blob *packetData)
+{
+    int offset = 6; // 4 for TL, 2 for T of the name
+    uint16_t len = ((uint16_t)(packetData->bytes[offset]) << 8) | (uint16_t)(packetData->bytes[offset + 1]);
+    return (size_t) len;
+}
+
+static size_t
+_getNameIndex(blob *packetData)
+{
+    return 8; // 8 + 4 + 4
+}
+
+static size_t
+_getContentHashIndex(blob *packetData) // skip past the name
+{
+    return _getNameIndex(packetData) + _getNameLength(packetData) + 4;
+}
+
+static size_t
+_getContentHashLength(blob *packetData) // skip past the name
+{
+    int offset = _getNameIndex(packetData) + _getNameLength(packetData) + 2;
+    uint16_t len = ((uint16_t)(packetData->bytes[offset]) << 8) | (uint16_t)(packetData->bytes[offset + 1]);
+    return (size_t) len;
+}
+
 static blob *
 _readName(blob *packetData)
 {
-    return NULL;
+    size_t len = _getNameLength(packetData);
+    blob *b = (blob *) malloc(sizeof(blob));
+    b->bytes = malloc(len);
+    b->length = len;
+    memcpy(b->bytes, packetData->bytes + _getNameIndex(packetData), len);
+    return b;
 }
 
 static blob *
 _readContentObjectHash(blob *packetData)
 {
-    return NULL;
+    size_t len = _getContentHashLength(packetData);
+    blob *b = (blob *) malloc(sizeof(blob));
+    b->bytes = malloc(len);
+    b->length = len;
+    memcpy(b->bytes, packetData->bytes + _getContentHashIndex(packetData), len);
+    return b;
 }
 
 int
 main(int argc, char **argv)
 {
-    socklen_t clientlen;
-    struct sockaddr_in clientAddress;
-    struct hostent *hostp;
-    int numBytesReceived;
-    UDPServer server;
+    FILE *fp = fopen("data_int", "rb");
 
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <Server Port> <Directory>\n", argv[0]);
-        exit(1);
-    }
+    uint8_t header[8];
 
-    // Setup the server port
-    server.port = atoi(argv[1]);
+    if (fp != NULL) {
+        // read the header
+        fread(header, 1, 8, fp);
 
-    // Load the contents of the directory into memory
-    _loadDirectory(&server, argv[2]);
+        // printf("%d %d\n", header[2], header[3]);
 
-    if ((server.socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        LogFatal("socket() failed");
-    }
+        // get the size
+        uint16_t len = ((uint16_t)(header[2]) << 8) | (uint16_t)(header[3]);
 
-    // set address reuse
-    int optval = 1;
-    setsockopt(server.socket, SOL_SOCKET, SO_REUSEADDR, (const void *) &optval , sizeof(int));
+        // read the packet into a blob
+        blob *pktBlob = malloc(sizeof(blob));
+        pktBlob->bytes = malloc(len);
+        pktBlob->length = len;
+        int numRead = fread(pktBlob->bytes, 1, len, fp);
 
-    bzero((char *) &server.serverAddress, sizeof(server.serverAddress));
-    server.serverAddress.sin_family = AF_INET;
-    server.serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.serverAddress.sin_port = htons(server.port);
+        printf("name length = %d\n", _getNameLength(pktBlob));
 
-    if (bind(server.socket, (struct sockaddr *) &(server.serverAddress), sizeof(server.serverAddress)) < 0) {
-        LogFatal("bind() failed");
-    }
+        printf("len = %d %d\n", len, numRead);
 
-    clientlen = sizeof(clientAddress);
-    char buffer[MTU];
-    bzero(buffer, MTU);
-
-    for (;;) {
-        numBytesReceived = recvfrom(server.socket, buffer, MTU, 0, (struct sockaddr *) &clientAddress, &clientlen);
-        if (numBytesReceived < 0) {
-            LogFatal("recvfrom() failed");
+        blob *name = _readName(pktBlob);
+        printf("name len=%d\n", name->length);
+        for (size_t i = 0; i < name->length; i++) {
+            putc(name->bytes[i], stdout);
         }
-
-        // TODO;
-        // 1) decode the packet to obtain the name and COH
-        // 2) index into the repo to get the packet
-        // 3) reply with the appropriate response
+        printf("rawr\n");
     }
 
-    close(server.socket);
 
-    return 0;
+    // socklen_t clientlen;
+    // struct sockaddr_in clientAddress;
+    // struct hostent *hostp;
+    // int numBytesReceived;
+    // UDPServer server;
+    //
+    // if (argc != 3) {
+    //     fprintf(stderr, "Usage: %s <Server Port> <Directory>\n", argv[0]);
+    //     exit(1);
+    // }
+    //
+    // // Setup the server port
+    // server.port = atoi(argv[1]);
+    //
+    // // Load the contents of the directory into memory
+    // _loadDirectory(&server, argv[2]);
+    //
+    // if ((server.socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    //     LogFatal("socket() failed");
+    // }
+    //
+    // // set address reuse
+    // int optval = 1;
+    // setsockopt(server.socket, SOL_SOCKET, SO_REUSEADDR, (const void *) &optval , sizeof(int));
+    //
+    // bzero((char *) &server.serverAddress, sizeof(server.serverAddress));
+    // server.serverAddress.sin_family = AF_INET;
+    // server.serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    // server.serverAddress.sin_port = htons(server.port);
+    //
+    // if (bind(server.socket, (struct sockaddr *) &(server.serverAddress), sizeof(server.serverAddress)) < 0) {
+    //     LogFatal("bind() failed");
+    // }
+    //
+    // clientlen = sizeof(clientAddress);
+    // char buffer[MTU];
+    // bzero(buffer, MTU);
+    //
+    // for (;;) {
+    //     numBytesReceived = recvfrom(server.socket, buffer, MTU, 0, (struct sockaddr *) &clientAddress, &clientlen);
+    //     if (numBytesReceived < 0) {
+    //         LogFatal("recvfrom() failed");
+    //     }
+    //
+    //     // TODO;
+    //     // 1) decode the packet to obtain the name and COH
+    //     // 2) index into the repo to get the packet
+    //     // 3) reply with the appropriate response
+    // }
+    //
+    // close(server.socket);
+    //
+    // return 0;
 }
