cdaec3d517324daa28b71740a8337a4a4c4ae59b
Christopher Wood <christopherwood07@gmail.com>
2015-12-13 10:59:34 -0800

    Pusher and receiver now read TLVs

--- src/pusher.c
+++ src/pusher.c
@@ -38,18 +38,36 @@ main(int argc, char** argv)
     Node *tail = (Node *) malloc(sizeof(Node));
     tail->next = NULL;
     tail->data = (blob *) malloc(sizeof(blob));
-    ssize_t read;
-    int lineno = 0;
-    while ((tail->data->bytes = (uint8_t *) fgetln(fp, &tail->data->length)) != NULL) {
-        printf("Read number %d\n", lineno);
-        lineno++;
+
+    uint8_t header[8];
+    size_t numRead = 1;
+
+    while (numRead > 0) {
+        bzero(header, 8);
+        numRead = fread(header, 1, 8, fp);
+        if (numRead == 0) {
+            break;
+        }
+
+        uint16_t len = ((uint16_t)(header[2]) << 8) | (uint16_t)(header[3]);
+        tail->data->bytes = malloc(len); // allocate packet header room
+        tail->data->length = len;
+        memcpy(tail->data->bytes, header, 8); // move the header into the packet
+        int numRead = fread(tail->data->bytes + 8, 1, len - 8, fp);
+        if (numRead != (len - 8)) {
+            fprintf(stderr, "Error: read the incorrect number of bytes, got %d expected %d\n", numRead, len - 8);
+            exit(1);
+        }
 
         Node *curr = (Node *) malloc(sizeof(Node));
+        curr->data = (blob *) malloc(sizeof(blob));
         curr->next = tail;
         tail = curr;
+
+        printf("Added entry of length %d\n", numRead + 8);
     }
 
-    Node *head = tail;
+    Node *head = tail->next;
 
     socketfd = socket(AF_INET, SOCK_DGRAM, 0);
     bzero(&servaddr, sizeof(servaddr));
@@ -59,35 +77,30 @@ main(int argc, char** argv)
 
     printf("Starting the piper\n");
 
+    int numPackets = 0;
     TimeBlock(stdout, {
 
         while (head != NULL) {
+            printf("Sending %d bytes\n", head->data->length);
             if (sendto(socketfd, head->data->bytes, head->data->length, 0,
                 (struct sockaddr *) &servaddr, sizeof(servaddr)) != head->data->length) {
                 LogFatal("send() failed");
             }
+
             head = head->next;
+            numPackets++;
 
-#if DEBUG
-            fprintf(stderr, "Received: \n");
-#endif
-
-            // bytesReceived = recv(socketfd, serverResponseBuffer, RCVBUFSIZE, 0);
-            // totalBytesRcvd += bytesReceived;
-            //
-            // // printf("%.*s", bytesReceived, serverResponseBuffer);
-            //
-            // if (bytesReceived < RCVBUFSIZE) {
-            //     break;
-            // }
+            bytesReceived = recv(socketfd, serverResponseBuffer, MTU, 0);
+            totalBytesRcvd += bytesReceived;
+            fprintf(stderr, "Received [%d]: \n", bytesReceived);
+            for (int i = 0; i < bytesReceived; i++) {
+                printf("%02x", serverResponseBuffer[i]);
+            }
+            printf("\n");
         }
-
-#if DEBUG
-        fprintf(stderr, "\n");
-#endif
-
     });
 
+    printf("Total packets: %d\n", numPackets);
     close(socketfd);
 
     return 0;
--- src/receiver.c
+++ src/receiver.c
@@ -15,7 +15,8 @@
 struct keyvalue;
 typedef struct keyvalue {
     uint8_t *key;
-    Node *value;
+    size_t keylen;
+    blob *value;
     struct keyvalue *next;
 } KeyValue;
 
@@ -29,113 +30,116 @@ typedef struct {
     KeyValue *head;
 } UDPServer;
 
-static void
-_loadDirectory(UDPServer *server, char *directory)
-{
-    DIR *fd;
-    if (NULL == (fd = opendir(directory))) {
-        fprintf(stderr, "Error: Failed to open input directory - %s\n", strerror(errno));
-        exit(1);
-    }
+// TODO: generalize this into a repo.
 
-    uint8_t buffer[FILE_BUFFER_LENGTH];
-
-    struct dirent* dirEntry;
-    while ((dirEntry = readdir(fd)))
-    {
-        if (!strcmp (dirEntry->d_name, "."))
-            continue;
-        if (!strcmp (dirEntry->d_name, ".."))
-            continue;
-
-        FILE *file = fopen(dirEntry->d_name, "rb");
-        if (file == NULL) {
-            fprintf(stderr, "Error: Failed to open entry file - %s\n", strerror(errno));
-            break;
-        }
-
-        Node *newNode = (Node *) malloc(sizeof(Node));
-        newNode->data = (blob *) malloc(sizeof(blob));
-        newNode->data->bytes = NULL;
-        newNode->data->length = 0;
-
-        size_t numBytesRead = FILE_BUFFER_LENGTH;
-        while (numBytesRead != 0) {
-            numBytesRead = fread(buffer, 1, FILE_BUFFER_LENGTH, file);
-            if (numBytesRead > 0) {
-                if (newNode->data->bytes == NULL) {
-                    newNode->data->bytes = (uint8_t *) malloc(numBytesRead);
-                } else {
-                    newNode->data->bytes = realloc((char **) newNode->data->bytes, newNode->data->length);
-                }
-                memcpy((newNode->data->bytes) + newNode->data->length, buffer, numBytesRead);
-                newNode->data->length += numBytesRead;
-            }
-        }
-
-        fclose(file);
-
-        KeyValue *kv = (KeyValue *) malloc(sizeof(KeyValue));
-        asprintf((char **) &kv->key, "%s", dirEntry->d_name);
-        kv->value = newNode;
-
-        kv->next = server->head;
-        server->head = kv;
-    }
-}
+// static void
+// _loadDirectory(UDPServer *server, char *directory)
+// {
+//     DIR *fd;
+//     if (NULL == (fd = opendir(directory))) {
+//         fprintf(stderr, "Error: Failed to open input directory - %s\n", strerror(errno));
+//         exit(1);
+//     }
+//
+//     uint8_t buffer[FILE_BUFFER_LENGTH];
+//
+//     struct dirent* dirEntry;
+//     while ((dirEntry = readdir(fd)))
+//     {
+//         if (!strcmp (dirEntry->d_name, "."))
+//             continue;
+//         if (!strcmp (dirEntry->d_name, ".."))
+//             continue;
+//
+//         FILE *file = fopen(dirEntry->d_name, "rb");
+//         if (file == NULL) {
+//             fprintf(stderr, "Error: Failed to open entry file - %s\n", strerror(errno));
+//             break;
+//         }
+//
+//         Node *newNode = (Node *) malloc(sizeof(Node));
+//         newNode->data = (blob *) malloc(sizeof(blob));
+//         newNode->data->bytes = NULL;
+//         newNode->data->length = 0;
+//
+//         size_t numBytesRead = FILE_BUFFER_LENGTH;
+//         while (numBytesRead != 0) {
+//             numBytesRead = fread(buffer, 1, FILE_BUFFER_LENGTH, file);
+//             if (numBytesRead > 0) {
+//                 if (newNode->data->bytes == NULL) {
+//                     newNode->data->bytes = (uint8_t *) malloc(numBytesRead);
+//                 } else {
+//                     newNode->data->bytes = realloc((char **) newNode->data->bytes, newNode->data->length);
+//                 }
+//                 memcpy((newNode->data->bytes) + newNode->data->length, buffer, numBytesRead);
+//                 newNode->data->length += numBytesRead;
+//             }
+//         }
+//
+//         fclose(file);
+//
+//         KeyValue *kv = (KeyValue *) malloc(sizeof(KeyValue));
+//         asprintf((char **) &kv->key, "%s", dirEntry->d_name);
+//         kv->keylen = strlen(dirEntry->d_name);
+//         kv->value = newNode;
+//
+//         kv->next = server->head;
+//         server->head = kv;
+//     }
+// }
 
 static size_t
-_getNameLength(blob *packetData)
+_getNameLength(uint8_t *buffer, size_t length)
 {
     int offset = 6; // 4 for TL, 2 for T of the name
-    uint16_t len = ((uint16_t)(packetData->bytes[offset]) << 8) | (uint16_t)(packetData->bytes[offset + 1]);
+    uint16_t len = ((uint16_t)(buffer[offset]) << 8) | (uint16_t)(buffer[offset + 1]);
     return (size_t) len;
 }
 
 static size_t
-_getNameIndex(blob *packetData)
+_getNameIndex(uint8_t *buffer, size_t length)
 {
     return 8; // 8 + 4 + 4
 }
 
 static size_t
-_getContentHashIndex(blob *packetData) // skip past the name
+_getContentHashIndex(uint8_t *buffer, size_t length) // skip past the name
 {
-    return _getNameIndex(packetData) + _getNameLength(packetData) + 4;
+    return _getNameIndex(buffer, length) + _getNameLength(buffer, length) + 4;
 }
 
 static size_t
-_getContentHashLength(blob *packetData) // skip past the name
+_getContentHashLength(uint8_t *buffer, size_t length) // skip past the name
 {
-    int offset = _getNameIndex(packetData) + _getNameLength(packetData) + 2;
-    uint16_t len = ((uint16_t)(packetData->bytes[offset]) << 8) | (uint16_t)(packetData->bytes[offset + 1]);
+    int offset = _getNameIndex(buffer, length) + _getNameLength(buffer, length) + 2;
+    uint16_t len = ((uint16_t)(buffer[offset]) << 8) | (uint16_t)(buffer[offset + 1]);
     return (size_t) len;
 }
 
 static blob *
-_readName(blob *packetData)
+_readName(uint8_t *buffer, size_t length)
 {
-    size_t len = _getNameLength(packetData);
+    size_t len = _getNameLength(buffer, length);
     blob *b = (blob *) malloc(sizeof(blob));
     b->bytes = malloc(len);
     b->length = len;
-    memcpy(b->bytes, packetData->bytes + _getNameIndex(packetData), len);
+    memcpy(b->bytes, buffer + _getNameIndex(buffer, length), len);
     return b;
 }
 
 static blob *
-_readContentObjectHash(blob *packetData)
+_readContentObjectHash(uint8_t *buffer, size_t length)
 {
-    size_t len = _getContentHashLength(packetData);
+    size_t len = _getContentHashLength(buffer, length);
     blob *b = (blob *) malloc(sizeof(blob));
     b->bytes = malloc(len);
     b->length = len;
-    memcpy(b->bytes, packetData->bytes + _getContentHashIndex(packetData), len);
+    memcpy(b->bytes, buffer + _getContentHashIndex(buffer, length), len);
     return b;
 }
 
-int
-main(int argc, char **argv)
+static void
+_testParsePacket()
 {
     FILE *fp = fopen("data_int", "rb");
 
@@ -156,70 +160,155 @@ main(int argc, char **argv)
         pktBlob->length = len;
         int numRead = fread(pktBlob->bytes, 1, len, fp);
 
-        printf("name length = %d\n", _getNameLength(pktBlob));
-
-        printf("len = %d %d\n", len, numRead);
-
-        blob *name = _readName(pktBlob);
-        printf("name len=%d\n", name->length);
+        blob *name = _readName(pktBlob->bytes, pktBlob->length);
         for (size_t i = 0; i < name->length; i++) {
             putc(name->bytes[i], stdout);
         }
-        printf("rawr\n");
+    }
+}
+
+static blob *
+_loadContent(UDPServer *server, blob *name, blob *hash)
+{
+    KeyValue *curr = server->head;
+    while (curr != NULL) {
+        if (curr->keylen == name->length) {
+            if (memcmp(curr->key, name->bytes, curr->keylen) == 0) {
+                return curr->value;
+            }
+        }
+        curr = curr->next;
+    }
+    return NULL;
+}
+
+static void
+_displayBlob(blob *blob)
+{
+    for (int i = 0; i < blob->length; i++) {
+        putc(blob->bytes[i], stdout);
+    }
+}
+
+static void
+_loadDataFromFile(UDPServer *server, char *fname)
+{
+    FILE *fp = fopen(fname, "rb");
+
+    if (fp == NULL) {
+        return;
     }
 
+    uint8_t header[8];
+    size_t numRead = 1;
 
-    // socklen_t clientlen;
-    // struct sockaddr_in clientAddress;
-    // struct hostent *hostp;
-    // int numBytesReceived;
-    // UDPServer server;
-    //
-    // if (argc != 3) {
-    //     fprintf(stderr, "Usage: %s <Server Port> <Directory>\n", argv[0]);
-    //     exit(1);
-    // }
-    //
-    // // Setup the server port
-    // server.port = atoi(argv[1]);
-    //
-    // // Load the contents of the directory into memory
-    // _loadDirectory(&server, argv[2]);
-    //
-    // if ((server.socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    //     LogFatal("socket() failed");
-    // }
-    //
-    // // set address reuse
-    // int optval = 1;
-    // setsockopt(server.socket, SOL_SOCKET, SO_REUSEADDR, (const void *) &optval , sizeof(int));
-    //
-    // bzero((char *) &server.serverAddress, sizeof(server.serverAddress));
-    // server.serverAddress.sin_family = AF_INET;
-    // server.serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    // server.serverAddress.sin_port = htons(server.port);
-    //
-    // if (bind(server.socket, (struct sockaddr *) &(server.serverAddress), sizeof(server.serverAddress)) < 0) {
-    //     LogFatal("bind() failed");
-    // }
-    //
-    // clientlen = sizeof(clientAddress);
-    // char buffer[MTU];
-    // bzero(buffer, MTU);
-    //
-    // for (;;) {
-    //     numBytesReceived = recvfrom(server.socket, buffer, MTU, 0, (struct sockaddr *) &clientAddress, &clientlen);
-    //     if (numBytesReceived < 0) {
-    //         LogFatal("recvfrom() failed");
-    //     }
-    //
-    //     // TODO;
-    //     // 1) decode the packet to obtain the name and COH
-    //     // 2) index into the repo to get the packet
-    //     // 3) reply with the appropriate response
-    // }
-    //
-    // close(server.socket);
-    //
-    // return 0;
+    while (numRead > 0) {
+        numRead = fread(header, 1, 8, fp);
+        if (numRead == 0) {
+            break;
+        }
+
+        uint16_t len = ((uint16_t)(header[2]) << 8) | (uint16_t)(header[3]);
+        blob *pktBlob = malloc(sizeof(blob));
+        pktBlob->bytes = malloc(len); // allocate packet header room
+        pktBlob->length = len;
+        memcpy(pktBlob->bytes, header, 8); // move the header into the packet
+        int numRead = fread(pktBlob->bytes + 8, 1, len - 8, fp);
+
+        // Extract the name and hash
+        blob *name = _readName(pktBlob->bytes + 8, len - 8);
+        blob *hash = _readContentObjectHash(pktBlob->bytes + 8, len - 8);
+
+        KeyValue *kv = (KeyValue *) malloc(sizeof(KeyValue));
+        kv->key = malloc(name->length);
+        memcpy(kv->key, name->bytes, name->length);
+        kv->keylen = name->length;
+        kv->value = pktBlob;
+
+        printf("Repo addition:");
+        _displayBlob(name);
+        printf("\n");
+        for (int i = 0; i < pktBlob->length; i++) {
+            printf("%02x", pktBlob->bytes[i]);
+        }
+        printf("\n");
+
+        kv->next = server->head;
+        server->head = kv;
+    }
+}
+
+int
+main(int argc, char **argv)
+{
+    socklen_t clientlen;
+    struct sockaddr_in clientAddress;
+    struct hostent *hostp;
+    int numBytesReceived;
+    UDPServer server;
+
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <Server Port> <Directory>\n", argv[0]);
+        exit(1);
+    }
+
+    // Setup the server port
+    server.port = atoi(argv[1]);
+
+    // Load the contents of the directory into memory
+    _loadDataFromFile(&server, argv[2]);
+
+    if ((server.socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        LogFatal("socket() failed");
+    }
+
+    // set address reuse
+    int optval = 1;
+    setsockopt(server.socket, SOL_SOCKET, SO_REUSEADDR, (const void *) &optval , sizeof(int));
+
+    bzero((char *) &server.serverAddress, sizeof(server.serverAddress));
+    server.serverAddress.sin_family = AF_INET;
+    server.serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.serverAddress.sin_port = htons(server.port);
+
+    if (bind(server.socket, (struct sockaddr *) &(server.serverAddress), sizeof(server.serverAddress)) < 0) {
+        LogFatal("bind() failed");
+    }
+
+    clientlen = sizeof(clientAddress);
+    uint8_t buffer[MTU];
+    for (;;) {
+        bzero(buffer, MTU);
+        numBytesReceived = recvfrom(server.socket, buffer, MTU, 0, (struct sockaddr *) &clientAddress, &clientlen);
+        if (numBytesReceived < 0) {
+            LogFatal("recvfrom() failed");
+        } else {
+            printf("Received %d bytes\n", numBytesReceived);
+        }
+
+        // 1. get the name of the packet
+        uint16_t len = ((uint16_t)(buffer[2]) << 8) | (uint16_t)(buffer[3]);
+        blob *name = _readName(buffer + 8, len - 8);
+        blob *hash = _readContentObjectHash(buffer + 8, len - 8);
+
+        // 2) index into the repo to get the packet
+        blob *content = _loadContent(&server, name, hash);
+        if (content != NULL) {
+            printf("Sending [%d]:\n", content->length);
+            for (int i = 0; i < content->length; i++) {
+                printf("%02x", content->bytes[i]);
+            }
+            printf("\n");
+            if (sendto(server.socket, content->bytes, content->length, 0,
+            	(struct sockaddr *) &clientAddress, clientlen) < 0) {
+                LogFatal("Error sending content object response\n");
+            }
+        } else {
+            printf("NOT FOUND!?!?!?!\n");
+        }
+    }
+
+    close(server.socket);
+
+    return 0;
 }
